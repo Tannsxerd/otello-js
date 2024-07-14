0eaa21640fdb9d22e8b013e8fcb2d5bac26e8564
tansxerd1231 <tansxerd1231@gmail.com>
2024-07-14 16:39:55 +0700

    Your descriptive commit message

--- otello.js
+++ otello.js
@@ -1,3 +1,28 @@
-document.write("otello")
-console.log("12345")
-console.log()
+class Othello {
+    constructor() {
+        this.board =  this.create2DArray(8,8)
+        this.int_piece()
+    }
+    int_piece(){
+        this.board[3][3] = "white"
+        this.board[4][4] = "white"
+        this.board[3][4] = "black"
+        this.board[4][3] = "black"
+    }
+    create2DArray(row, col) {
+        let array = [];
+        for (let i = 0; i < row; i++) {
+            array[i] = [];
+            for (let j = 0; j < col; j++) {
+                array[i][j] = null; // Example initialization, replace with your logic
+            }
+        } 
+        return array; 
+    }
+    user_input(){
+        
+    }
+}
+
+let game = new Othello();
+console.log(game.board)
